e22ba99aadcf87c984c4313020f7e288ec81efab
Daniel Rückert García <daniel@betterplaceapp.com>
2022-07-06 13:34:14 +0200

    add helper methods to cameras controller

--- src/controllers/cameras/cameras.py
+++ src/controllers/cameras/cameras.py
@@ -1,13 +1,58 @@
+from typing import Optional
 from v4l2py.device import iter_video_capture_devices
 import gphoto2 as gp
+from src.controllers.cameras.camera import CameraController
+from src.controllers.cameras.gphoto import GphotoController
+from src.controllers.cameras.v4l2 import V4l2Controller
+from src.config.camera import CameraConfig
 
 from src.models.camera import Camera, CameraType
+
 
 def get_cameras() -> "list[Camera]":
     cameras = []
     v4l2_cameras = iter_video_capture_devices()
-    cameras.extend([Camera(CameraType.V4L2, c.info.card, str(c.filename), None) for c in v4l2_cameras])
+    cameras.extend(
+        [
+            Camera(
+                type=CameraType.V4L2,
+                name=c.info.card,
+                path=str(c.filename),
+                settings=get_camera_settings(c.info.card, str(c.filename)),
+            )
+            for c in v4l2_cameras
+        ]
+    )
     gphoto2_cameras = gp.Camera.autodetect()
-    cameras.extend([Camera(CameraType.GPHOTO2, c[0], str(c.filename), None) for c in gphoto2_cameras])
+    cameras.extend(
+        [
+            Camera(
+                type=CameraType.GPHOTO2,
+                name=c[0],
+                path=c[1],
+                settings=get_camera_settings(c[0], c[1]),
+            )
+            for c in gphoto2_cameras
+        ]
+    )
 
     return cameras
+
+
+def get_camera_settings(name: str, path: str) -> Optional[CameraConfig]:
+    return None
+
+
+def get_camera(camera_id: int) -> Camera:
+    cameras = get_cameras()
+    if len(cameras) < camera_id + 1:
+        raise ValueError(f"Can't find camera with id {camera_id}")
+    return cameras[camera_id]
+
+
+def get_camera_controller(camera: Camera) -> type[CameraController]:
+    if camera.type == CameraType.V4L2:
+        return V4l2Controller
+    elif camera.type == CameraType.GPHOTO2:
+        return GphotoController
+    raise ValueError(f"Couldn't find controller for {camera.type}")
