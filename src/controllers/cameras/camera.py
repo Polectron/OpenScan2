dc717198d82fb8b9d731c78037e011791da2ce4e
Daniel Rückert García <daniel@betterplaceapp.com>
2022-07-06 13:33:44 +0200

    rename method picture to photo, receive camera and return bytesio

--- src/controllers/cameras/camera.py
+++ src/controllers/cameras/camera.py
@@ -1,13 +1,16 @@
 import abc
+import io
+
+from src.models.camera import Camera
 
 
 class CameraController(abc.ABC):
     @staticmethod
     @abc.abstractmethod
-    def picture():
+    def photo(camera: Camera) -> io.BytesIO:
         raise NotImplementedError
 
     @staticmethod
     @abc.abstractmethod
-    def preview():
+    def preview(camera: Camera) -> io.BytesIO:
         raise NotImplementedError
