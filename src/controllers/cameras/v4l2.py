ae01f644cffc06d58be07e988384d5503afb1337
Daniel Rückert García <daniel@betterplaceapp.com>
2022-07-06 13:41:12 +0200

    implement v4l2 controller

--- src/controllers/cameras/v4l2.py
+++ src/controllers/cameras/v4l2.py
@@ -1,11 +1,17 @@
+import io
 from v4l2py import Device
+
+from src.models.camera import Camera
 
 from .camera import CameraController
 
 
 class V4l2Controller(CameraController):
-    def picture():
-        raise NotImplementedError
+    def photo(camera: Camera) -> io.BytesIO:
+        with Device.from_id(0) as v4l2_camera:
+            v4l2_camera_stream = iter(v4l2_camera)
+            next(v4l2_camera_stream)  # first frame can be garbage
+            return io.BytesIO(next(v4l2_camera_stream))
 
-    def preview():
-        raise NotImplementedError
+    def preview(camera: Camera) -> io.BytesIO:
+        return V4l2Controller.photo(camera)
